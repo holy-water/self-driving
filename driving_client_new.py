64f1c9b4f7eac7d629b21b100b1214d3ae58cb45
Seongsu Kim <spiraled@naver.com>
2019-08-03 14:28:35 +0900

    도로 분할 계수 수정 및 장애물 범위 확대

--- driving_client_new.py
+++ driving_client_new.py
@@ -17,38 +17,38 @@ class DrivingClient(DrivingController):
         self.collision_flag = False
 
         #
-        # Editing area ends
-        # ==========================================================#
+        #         # Editing area ends
+        #         # ==========================================================#
         super().__init__()
         # 제한 속도 할당
         self.limit_speed = 100
         # 도로 범위 할당
-        self.track_range = int(self.half_road_limit) if int(self.half_road_limit) % 2 != 0 else int(self.half_road_limit) - 1
+        track_range = int((self.half_road_limit - 1.25) * 2 / 2.5)
+        self.track_range = track_range if track_range % 2 != 0 else track_range - 1
         # 이전 도로 번호 할당
         self.previous_track_number = int(self.track_range / 2) + 1
 
     def control_driving(self, car_controls, sensing_info):
-
         # =========================================================== #
         # Area for writing code about driving rule ================= #
         # =========================================================== #
         # Editing area starts from here
         #
 
-        # logging.debug("=========================================================")
-        # logging.debug("to middle: {}".format(sensing_info.to_middle))
-        #
-        # logging.debug("collided: {}".format(sensing_info.collided))
-        # logging.debug("car speed: {} km/h".format(sensing_info.speed))
-        #
-        # logging.debug("is moving forward: {}".format(sensing_info.moving_forward))
-        # logging.debug("moving angle: {}".format(sensing_info.moving_angle))
-        # logging.debug("lap_progress: {}".format(sensing_info.lap_progress))
-        #
-        # logging.debug("track_forward_angles: {}".format(sensing_info.track_forward_angles))
-        # logging.debug("track_forward_obstacles: {}".format(sensing_info.track_forward_obstacles))
-        # logging.debug("opponent_cars_info: {}".format(sensing_info.opponent_cars_info))
-        # logging.debug("=========================================================")
+        logging.debug("=========================================================")
+        logging.debug("to middle: {}".format(sensing_info.to_middle))
+
+        logging.debug("collided: {}".format(sensing_info.collided))
+        logging.debug("car speed: {} km/h".format(sensing_info.speed))
+
+        logging.debug("is moving forward: {}".format(sensing_info.moving_forward))
+        logging.debug("moving angle: {}".format(sensing_info.moving_angle))
+        logging.debug("lap_progress: {}".format(sensing_info.lap_progress))
+
+        logging.debug("track_forward_angles: {}".format(sensing_info.track_forward_angles))
+        logging.debug("track_forward_obstacles: {}".format(sensing_info.track_forward_obstacles))
+        logging.debug("opponent_cars_info: {}".format(sensing_info.opponent_cars_info))
+        logging.debug("=========================================================")
 
         ###########################################################################
 
@@ -57,7 +57,8 @@ class DrivingClient(DrivingController):
         # 속도 조절
         car_controls.throttle = 1 if sensing_info.speed < self.limit_speed else 0
 
-        # logging.debug("steering:{}, throttle:{}, brake:{}".format(car_controls.steering, car_controls.throttle, car_controls.brake))
+        logging.debug("steering:{}, throttle:{}, brake:{}".format(car_controls.steering, car_controls.throttle,
+                                                                  car_controls.brake))
 
         #
         # Editing area ends
@@ -79,11 +80,11 @@ class DrivingClient(DrivingController):
 def calculate_steering(self, sensing_info):
     # 도로 번호에 맞는 중앙값 선택
     track_number = select_track_number(self, sensing_info)
-    to_middle = (track_number - (int(self.track_range / 2) + 1)) * self.track_range * 0.5
+    to_middle = (track_number - (int(self.track_range / 2) + 1)) * self.track_range * 0.6
     # 현재 중앙값에 맞는 핸들값 할당
-    middle = -(sensing_info.to_middle + to_middle) / (self.half_road_limit * self.track_range)
+    middle = -(sensing_info.to_middle + to_middle) / (self.track_range * self.track_range)
     # 현재 도로 각도에 맞는 핸들값 할당
-    angle = -(sensing_info.moving_angle - sensing_info.track_forward_angles[0]) / 60
+    angle = -(sensing_info.moving_angle - sensing_info.track_forward_angles[0]) / 50
     # 코너에서 핸들값 가중치 할당
     angle_weight = calculate_angle_weight(sensing_info, angle)
     return middle + angle + angle_weight
@@ -107,9 +108,9 @@ def select_track_number(self, sensing_info):
                     elif self.previous_track_number - (mark * index) not in disable_track_number_list:
                         track_number = self.previous_track_number - (mark * index)
                         break
-    #     print("disable_track_number_list : {}".format(disable_track_number_list))
-    # print("previous_track_number : {}".format(self.previous_track_number))
-    # print("track_number : {}".format(track_number))
+        print("disable_track_number_list : {}".format(disable_track_number_list))
+    print("previous_track_number : {}".format(self.previous_track_number))
+    print("track_number : {}".format(track_number))
     # 현재 도로 번호 저장
     self.previous_track_number = track_number
     return track_number
@@ -119,8 +120,22 @@ def get_disable_track_number_list(sensing_info, middle_number):
     disable_track_number_list = []
     for obstacle in sensing_info.track_forward_obstacles:
         if obstacle['dist'] < sensing_info.speed * 5 / 18 * 1.5:
-            for index in range(middle_number - 1):
-                if abs(obstacle['to_middle']) < 1 + (3 * index):
+            for index in range(middle_number):
+                if abs(obstacle['to_middle']) == 0.04:
+                    print("들어옴")
+                if abs(obstacle['to_middle']) - 1 < 1.25 + (2.5 * index):
+                    if abs(obstacle['to_middle']) == 0.04:
+                        print("index -1: {}".format(index))
+                    if obstacle['to_middle'] > 0:
+                        disable_track_number_list.append(middle_number - index)
+                        break
+                    else:
+                        disable_track_number_list.append(middle_number + index)
+                        break
+            for index in range(middle_number):
+                if abs(obstacle['to_middle']) + 1 < 1.25 + (2.5 * index):
+                    if abs(obstacle['to_middle']) == 0.04:
+                        print("index +1: {}".format(index))
                     if obstacle['to_middle'] > 0:
                         disable_track_number_list.append(middle_number - index)
                         break
@@ -135,8 +150,8 @@ def get_disable_track_number_list(sensing_info, middle_number):
 def calculate_angle_weight(sensing_info, angle):
     weight = 0
     for i in range(2):
-        if abs(sensing_info.track_forward_angles[i]) > 18:
-            weight = (angle * abs(sensing_info.track_forward_angles[i])) / 18
+        if abs(sensing_info.track_forward_angles[i]) > 23:
+            weight = (angle * abs(sensing_info.track_forward_angles[i])) / 23
             if sensing_info.track_forward_angles[i] * angle < 0:
                 weight *= -1
             break
